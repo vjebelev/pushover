67b4f1f2784a4576966f2d33d63e2a5c8b590509
Vlad Jebelev <vlad@jebelev.com>
2019-07-18 21:39:10 -0400

    cleanup mix.exs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,21 @@ defmodule Pushover.MixProject do
       version: @version,
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+    ]
+  end
+
+  def description do
+    "Pushover API Client for Elixir"
+  end
+
+  def package do
+    [
+      name: "pushover elixir",
+      maintainers: ["Vlad Jebelev"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/vjebelev/pushover"}
     ]
   end
 
@@ -23,10 +37,13 @@ defmodule Pushover.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.4"},
-      {:jason, "~> 1.1"},
+      #      {:httpoison, "~> 1.4"},
+      #{:jason, "~> 1.1"},
       {:tesla, "~> 1.2.1"},
-      {:google_gax, "~> 0.1.3"}
+      {:google_gax, "~> 0.1.3"},
+
+      # Docs
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
     ]
   end
 end
