0fe2713c27439828367c04d86bfaf2dba096e2f0
Vlad Jebelev <vlad@jebelev.com>
2019-07-24 20:47:56 -0400

    cleaning out scopes

--- lib/pushover/connection.ex
+++ lib/pushover/connection.ex
@@ -6,9 +6,6 @@ defmodule Pushover.Connection do
   @type t :: Tesla.Env.client()
 
   use GoogleApi.Gax.Connection,
-    scopes: [
-      "https://api.pushover.net/1"
-    ],
     otp_app: :pushover,
     base_url: "https://api.pushover.net/1"
 end
